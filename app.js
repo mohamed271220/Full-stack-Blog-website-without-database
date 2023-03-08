3570e38778b9231e5f2c86550561932d6e0aa94b
mohamed271220 <mohamedaprahym@gmail.com>
2023-03-08 14:29:44 +0200

    added a local database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,19 +13,35 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// mongoose.set("strictQuery", false)
+// const PORT = process.env.PORT || 3000;
 
-const posts = []
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", { useNewUrlParser: true });
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 
 
 
 app.get('/', (req, res) => {
-  res.render("home", { homeStartingContent, posts });
-
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -36,37 +54,41 @@ app.get('/compose', (req, res) => {
   res.render("compose");
 });
 app.post('/compose', (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postName,
     content: req.body.thePost,
-  };
-  posts.push(post)
-  res.redirect('/')
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 
 // express routing 
-app.get("/posts/:post", (req, res) => {
+app.get("/posts/:postid", (req, res) => {
+  const requestedPostId = req.params.postid;
   const requestTitle = _.lowerCase(req.params.post);
-  posts.map((post) => {
-    if (requestTitle === _.lowerCase(post.title)) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
+  // posts.map((post) => {
+  //   if (requestTitle === _.lowerCase(post.title)) {
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+
+    res.render("post", {
+
+      title: post.title,
+
+      content: post.content
+
+    });
 
   });
-
-});
-
+})
 
 
+// connectDB().then(() => {
 
-
-
-
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
 });
